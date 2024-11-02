33c7c26046fdc777748b825beef71d40f8aa2474
andresgalrod21 <acamilogalindo@ucundinamarca.edu.co>
2024-11-01 20:20:41 -0500

    Nav buttons full

--- js/back.js
+++ js/back.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const backButton1 = document.getElementById('back-button-1');
     const backButton2 = document.getElementById('back-button-2');
     const backButton3 = document.getElementById('back-button-3');
     const actionButtons = document.getElementById('action-buttons');
@@ -7,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const sections = [
         "diagnosticos", "groups", "folios", "headquarters", "incomes", "medications",
         "permissions-groups", "permissions", "staff", "tipdocs", "logs", "score", "users", 
-        "patients", "supplies-patients", "signs", "nurse-note-section", "patient-records"
+        "patients", "supplies-patients", "signs", "nurse-note-section", "patient-records", "mantadmin", "notasenf"
     ];
 
     // Función para ocultar todas las secciones
@@ -22,6 +23,16 @@ document.addEventListener('DOMContentLoaded', function() {
     function showActionButtons() {
         hideAllSections();
         actionButtons.style.display = 'block';
+        backButton2.style.display = 'none';
+        backButton3.style.display = 'none';
+        backButton1.style.display = 'block';
+    }
+
+    // Evento para refrescar la página en el botón 1
+    if (backButton1) {
+        backButton1.addEventListener('click', function() {
+            location.reload(); // Refresca la página
+        });
     }
 
     // Agregar evento al botón 2
